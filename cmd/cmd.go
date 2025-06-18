02c1863b2b172c99f6bdea3c297757ecde29743b
Ondrej Sika <ondrej@ondrejsika.com>
2025-06-18 07:06:43 +0200

    feat: Add slu tools to master tools (to be used for master tools mon as slu tools mon)

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -6,6 +6,7 @@ import (
 	slr_root "github.com/sikalabs/slr/cmd/root"
 	_ "github.com/sikalabs/slu/cmd"
 	slu_root "github.com/sikalabs/slu/cmd/root"
+	slu_tools "github.com/sikalabs/slu/cmd/tools"
 	_ "github.com/sikalabs/tergum/cmd"
 	tergum_root "github.com/sikalabs/tergum/cmd/root"
 	"github.com/spf13/cobra"
@@ -21,6 +22,7 @@ func init() {
 	Cmd.AddCommand(slu_root.RootCmd)
 	Cmd.AddCommand(slr_root.Cmd)
 	Cmd.AddCommand(tergum_root.Cmd)
+	Cmd.AddCommand(slu_tools.Cmd)
 }
 
 func Execute() {
